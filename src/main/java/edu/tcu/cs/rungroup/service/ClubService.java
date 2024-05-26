39c211cf1033081e0fff21f4b188dbaa5f11c262
Vinh Nguyen <vinh29012005@gmail.com>
2024-05-26 16:39:56 +0700

    from ClubController, create methods and link to html files, use Bootstrap template to create patterns

--- src/main/java/edu/tcu/cs/rungroup/service/ClubService.java
+++ src/main/java/edu/tcu/cs/rungroup/service/ClubService.java
@@ -1,9 +1,12 @@
 package edu.tcu.cs.rungroup.service;
 
 import edu.tcu.cs.rungroup.dto.ClubDto;
+import edu.tcu.cs.rungroup.models.Club;
 
 import java.util.List;
 
 public interface ClubService {
     List<ClubDto> findAllClubs();
+    Club saveClub(Club club);
+
 }
